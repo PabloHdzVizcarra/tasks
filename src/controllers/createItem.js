5ad8a00622e084c2a11c5150d806f4deb20bbf4f
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-04-21 12:37:15 -0500

    fix test parameters to save in database

--- src/controllers/createItem.js
+++ src/controllers/createItem.js
@@ -2,13 +2,14 @@ const ClientPostgreSQL = require("../database/ClientPostgres");
 
 function createItem(req, res) {
   const { state, text } = req.body;
-  if (!state || !text) {
+  if (!text) {
     res.status(400).json({ error: "you must pass all the correct data" });
+    return;
   }
 
   ClientPostgreSQL.insert({ state, text });
 
-  res.json({ message: "insert data is successfully" });
+  res.json({ message: `insert task ${text} successfully` });
 }
 
 module.exports = createItem;
