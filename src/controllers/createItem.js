da342bb43085a0418a8a0017724e308b87ea01a4
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-04-21 12:31:30 -0500

    fix not send parameters

--- src/controllers/createItem.js
+++ src/controllers/createItem.js
@@ -2,10 +2,13 @@ const ClientPostgreSQL = require("../database/ClientPostgres");
 
 function createItem(req, res) {
   const { state, text } = req.body;
+  if (!state || !text) {
+    res.status(400).json({ error: "you must pass all the correct data" });
+  }
 
   ClientPostgreSQL.insert({ state, text });
 
-  res.json({ message: "example" });
+  res.json({ message: "insert data is successfully" });
 }
 
 module.exports = createItem;
