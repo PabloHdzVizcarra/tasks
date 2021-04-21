ec9be1a9143f3d6f4dbd89e063d28b08adb28f2f
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-04-20 23:03:03 -0500

    feat server send html file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const { app } = require("./server");
+const path = require("path");
 
 const PORT = process.env.PORT || 9000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App listen in port ${PORT}`);
